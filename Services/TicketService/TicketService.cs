6d8d4b879d689c888ffe344f1eaba9f289584074
XynthiaKlomp <xynthiaklomp@gmail.com>
2023-06-12 13:01:29 +0200

    changed get least amount responisble for.

--- Services/TicketService/TicketService.cs
+++ Services/TicketService/TicketService.cs
@@ -13,7 +13,6 @@ namespace SupportTicketSystem.Services.TicketService
 {
     public class TicketService : ITicketService
     {
-
         public DataContext _dataContext { get; }
         public IMapper _mapper { get; }
         public IConversationService _conversationService { get; }
@@ -51,13 +50,14 @@ namespace SupportTicketSystem.Services.TicketService
                 var ticket = await _dataContext.Ticket.FirstAsync(t => t.Id == id);
                 Guard.Against.Null(ticket);
 
-                // send mail to user of ticket 
+                // send mail to user when Ticket is being archived
                 var conversation = await _dataContext.Conversation.FirstOrDefaultAsync(c => c.TicketId == ticket.Id);
                 Guard.Against.Null(conversation);
+
                 var message = $"\n Hello, \n Your ticket has been deleted. This was the ticket number {ticket.Id}. \n Greetings, {ticket.ResponsibleFor?.Name} \n Secret View";
                 await _conversationService.UpdateLog(conversation.Id, message);
 
-                //delete ticket by id.
+                //ticket is being archieved
                 ticket.Archived = DateTime.Now;
 
                 await _dataContext.SaveChangesAsync();
@@ -66,6 +66,7 @@ namespace SupportTicketSystem.Services.TicketService
             }
             catch (Exception ex)
             {
+                //send message when it goes wrong.
                 serviceResponse.Succes = false;
                 serviceResponse.Message = ex.Message;
             }
@@ -75,7 +76,6 @@ namespace SupportTicketSystem.Services.TicketService
 
         public async Task<ServiceResponse<List<GetTicketDto>>> GetAll()
         {
-            //get all tickets
             var serviceResponse = new ServiceResponse<List<GetTicketDto>>();
 
             var tickets = await _dataContext.Ticket.GetTicketDtoFromQuery(_mapper);
@@ -88,8 +88,6 @@ namespace SupportTicketSystem.Services.TicketService
 
         public async Task<ServiceResponse<GetTicketDto>> GetById(int id)
         {
-            //get ticket by id
-
             var serviceResponse = new ServiceResponse<GetTicketDto>();
 
             var ticket = await _dataContext.Ticket.GetById(id);
@@ -103,11 +101,11 @@ namespace SupportTicketSystem.Services.TicketService
         {
             var serviceResponse = new ServiceResponse<GetTicketDto>();
 
-            //update ticket by id.
             try
             {
                 var ticket = await _dataContext.Ticket.GetById(id);
                 Guard.Against.Null(ticket);
+
                 ticket = _mapper.Map<UpdateTicketDto, Ticket>(updateTicket, ticket);
 
                 await _dataContext.SaveChangesAsync();
@@ -130,14 +128,13 @@ namespace SupportTicketSystem.Services.TicketService
 
             try
             {
-                //update ticket status
                 var ticket = await _dataContext.Ticket.GetById(id);
                 Guard.Against.Null(ticket);
 
                 ticket.Status = updateTicket.Status;
                 ticket.ResponsibleFor = await _dataContext.User.FirstOrDefaultAsync(x => x.Id == ticket.ResponsibleForID);
 
-                // send mail to user of ticket 
+                // send mail to user of ticket that ticket status has updated 
                 var conversation = await _dataContext.Conversation.FirstOrDefaultAsync(c => c.TicketId == ticket.Id);
                 Guard.Against.Null(conversation);
 
@@ -161,7 +158,6 @@ namespace SupportTicketSystem.Services.TicketService
 
         public async Task<ServiceResponse<List<GetJoinUserTicketDto>>> AddUsersInvolved(AddJoinUserTicketDto newJoinUserTicket)
         {
-            // add users involved to the tickeet
             var serviceResponse = new ServiceResponse<List<GetJoinUserTicketDto>>();
 
             try
@@ -171,9 +167,10 @@ namespace SupportTicketSystem.Services.TicketService
 
                 joinUserTicket.InvolvedUser = await _dataContext.User.FirstOrDefaultAsync(u => u.Id == joinUserTicket.UserId);
                 joinUserTicket.Ticket = await _dataContext.Ticket.FirstOrDefaultAsync(t => t.Id == joinUserTicket.TicketId);
+
                 await _dataContext.AddAsync(joinUserTicket);
                 
-                // send mail to user of ticket
+                // send mail to user of ticket about that a user has been involved in ticket.
                 var conversation = await _dataContext.Conversation.FirstOrDefaultAsync(c => c.TicketId == joinUserTicket.Ticket.Id);
                 Guard.Against.Null(conversation);
 
@@ -190,7 +187,6 @@ namespace SupportTicketSystem.Services.TicketService
                 serviceResponse.Succes = false;
                 serviceResponse.Message = ex.Message;
             }
-
             return serviceResponse;
         }
 
@@ -198,7 +194,6 @@ namespace SupportTicketSystem.Services.TicketService
         {
             var serviceResponse = new ServiceResponse<GetTicketDto>();
 
-            //update the severity level of a ticket 1 level up.
             try
             {
                 var ticket = await _dataContext.Ticket.GetById(id);
@@ -223,7 +218,6 @@ namespace SupportTicketSystem.Services.TicketService
                 serviceResponse.Succes = false;
                 serviceResponse.Message = ex.Message;
             }
-
             return serviceResponse;
         }
 
@@ -231,7 +225,6 @@ namespace SupportTicketSystem.Services.TicketService
         {
             var serviceResponse = new ServiceResponse<GetTicketDto>();
 
-            //update the severity level of a ticket 1 level down.
             try
             {
                 var ticket = await _dataContext.Ticket.GetById(id);
@@ -256,7 +249,6 @@ namespace SupportTicketSystem.Services.TicketService
                 serviceResponse.Succes = false;
                 serviceResponse.Message = ex.Message;
             }
-
             return serviceResponse;
         }
 
@@ -264,7 +256,6 @@ namespace SupportTicketSystem.Services.TicketService
         {
             var serviceResponse = new ServiceResponse<GetTicketDto>();
 
-            //update ticket resposible user
             try
             {
                 var ticket = await _dataContext.Ticket.GetById(id);
@@ -282,55 +273,24 @@ namespace SupportTicketSystem.Services.TicketService
                 serviceResponse.Succes = false;
                 serviceResponse.Message = ex.Message;
             }
-
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<int>> GetLeastAmountResposibleFor()
         {
-            // get the user who is the least responsible for tickets.
             var serviceResponse = new ServiceResponse<int>();
 
-            List<User> Users = await _dataContext.User.ToListAsync();
-            Guard.Against.Null(Users);
             List<Ticket> Tickets = await _dataContext.Ticket.ToListAsync();
             Guard.Against.Null(Tickets);
-            List<List<int>> AmountResponsibleForPerUser = new List<List<int>>();
-            List<int> AmountResponsibleFor = new List<int>();
 
-            int userIdWithLeastAmount = 0;
-            
-            // looking how many tickets the users are responsible for
-            foreach (var user in Users)
-            {
-                int amountResponsibleFor = 0;
-                foreach (var ticket in Tickets)
-                {
-                    if (ticket.ResponsibleForID == user.Id)
-                    {
-                        amountResponsibleFor++;
-                    }
-                }
-                AmountResponsibleFor.Add(amountResponsibleFor);
+            //tickets get grouped by responsiblefor id. ordered by the count of tickets. returns first or default resposible for id.
+            var userIdWithLeastAmount = Tickets
+                .GroupBy(x => x.ResponsibleForID)
+                .OrderBy(x => x.Count())
+                .FirstOrDefault()?.Key;
+            Guard.Against.Null(userIdWithLeastAmount);
 
-
-                AmountResponsibleForPerUser.Add(new List<int> { user.Id, amountResponsibleFor });
-            }
-
-            AmountResponsibleFor.Sort();
-
-            // getting the user id which has the least amount of tickets responsible for
-            foreach (var amountResponsibleForPerUser in AmountResponsibleForPerUser)
-            {
-                if (amountResponsibleForPerUser.Last() == AmountResponsibleFor.First())
-                {
-                    userIdWithLeastAmount = amountResponsibleForPerUser.First();
-
-                    serviceResponse.Data = userIdWithLeastAmount;
-                    break;
-                }
-                
-            }
+            serviceResponse.Data = userIdWithLeastAmount.Value;
 
             return serviceResponse;
         }
